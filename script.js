0f96da52675ba4c4bd230912bd634ce8c6350cfa
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-03-30 15:55:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 //your JS code here. If required.
-function updateSize() {
+ function updateSize() {
         const width = window.innerWidth;
         const height = window.innerHeight;
         document.getElementById('width').textContent = width;
         document.getElementById('height').textContent = height;
     }
 
-    // Update size when the window is resized
     window.addEventListener('resize', updateSize);
 
-    // Initial update
-    updateSize();
+    updateSize(); // Initial call to set initial width and height
